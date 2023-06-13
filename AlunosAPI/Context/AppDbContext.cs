6b23c56e22c4d469e0a7be1f4de43715e2701992
torresmateus <mateustorres2017@gmail.com>
2023-06-12 22:26:21 -0300

    Projeto com os Models e os Repositories

---
+++ AlunosAPI/Context/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using AlunosAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlunosAPI.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Aluno> Alunos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Aluno>().HasData(
+                new Aluno
+                {
+                    Id = 1,
+                    Nome = "Fernando",
+                    Email = "laranja@gmail.com",
+                    Idade = 24
+                },
+                new Aluno
+                {
+                    Id = 2,
+                    Nome = "Roberto",
+                    Email = "capivara@gmail.com",
+                    Idade = 30
+                }
+                );
+        }
+    }
+}
---
+++ AlunosAPI/Controllers/AlunosController.cs
@@ -0,0 +1,33 @@
+﻿using AlunosAPI.Models;
+using AlunosAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlunosAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunosController : ControllerBase
+    {
+        private IAlunoService _alunoService;
+
+        public AlunosController(IAlunoService alunoService)
+        {
+            _alunoService = alunoService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IAsyncEnumerable<Aluno>>> GetAlunos()
+        {
+            try
+            {
+                var alunos = await _alunoService.GetAlunos();
+                return Ok(alunos);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao obter alunos");
+            }
+        }
+    }
+}
